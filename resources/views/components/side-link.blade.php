8892469960fd22c190f86bb8a518bca2d77da8c2
Ziyad Alwafie <zyalwfie@gmail.com>
2025-07-22 13:02:19 +0800

    feat: separate link component for sidebar

--- resources/views/components/side-link.blade.php
+++ resources/views/components/side-link.blade.php
@@ -1,8 +1,20 @@
-@props(['currentPage'])
+@props(['ariaCurrent' => false, 'href', 'current' => false])
+
+@if ($current)
+    @php
+        $classes =
+            'flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group bg-gray-300';
+        $arriaCurrent = 'page';
+    @endphp
+@else
+    @php
+        $classes =
+            'flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group';
+    @endphp
+@endif
 
 <li>
-    <a href="{{ $href }}"
-        class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ $currentPage ? 'bg-gray-300' : '' }}">
+    <a href="{{ $href }}" class="{{ $classes }}" aria-current="{{ $ariaCurrent }}">
         {{ $icon }}
         <span class="ml-3">{{ $slot }}</span>
     </a>
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -3,7 +3,7 @@
     aria-label="Sidenav" id="drawer-navigation">
     <div class="overflow-y-auto py-5 px-3 h-full bg-white dark:bg-gray-800">
         <ul class="space-y-2">
-            <x-side-link :href="{{ route('dashboard.facility.index') }}">
+            <x-side-link :href="route('dashboard.facility.index')" :current="Route::is('dashboard.facility.index*')">
                 <x-slot:icon>
                     <svg class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                         aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
@@ -14,6 +14,18 @@
                     </svg>
                 </x-slot:icon>
                 Fasilitas
+            </x-side-link>
+            <x-side-link :href="route('dashboard.review.index')" :current="Route::is('dashboard.review.index*')">
+                <x-slot:icon>
+                    <svg class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                        viewBox="0 0 24 24">
+                        <path fill-rule="evenodd"
+                            d="M2 6a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6Zm4.996 2a1 1 0 0 0 0 2h.01a1 1 0 1 0 0-2h-.01ZM11 8a1 1 0 1 0 0 2h6a1 1 0 1 0 0-2h-6Zm-4.004 3a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2h-.01ZM11 11a1 1 0 1 0 0 2h6a1 1 0 1 0 0-2h-6Zm-4.004 3a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2h-.01ZM11 14a1 1 0 1 0 0 2h6a1 1 0 1 0 0-2h-6Z"
+                            clip-rule="evenodd" />
+                    </svg>
+                </x-slot:icon>
+                Review
             </x-side-link>
         </ul>
     </div>
